d14e4bc34148b55e4ca5e7cd3dc04b7e440ffb45
Gerardo Castro Tamayo <gcastrot82@hotmail.com>
2022-02-25 09:47:00 -0500

    Caso 3: Calcular promedio con un conjunto de dos elementos (rojo)

--- tests/test_conjunto.py
+++ tests/test_conjunto.py
@@ -4,6 +4,15 @@ from KataTDD.conjunto import Conjunto
 class TestConjunto(unittest.TestCase):
 
     def test_conjunto_vacio(self):
+        conjunto = Conjunto([])
+        self.assertIsNone(conjunto.dar_promedio())
+        #self.assertEqual(conjunto.dar_promedio(),5)
+
+    def test_conjunto_un_elemento(self):
         conjunto = Conjunto([5])
         #self.assertIsNone(conjunto.dar_promedio(),5)
         self.assertEqual(conjunto.dar_promedio(),5)
+
+    def test_conjunto_dos_elementos(self):
+        conjunto = Conjunto([5,7])
+        self.assertEqual(conjunto.dar_promedio(),6)
